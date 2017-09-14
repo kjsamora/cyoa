21a149d0e65b6e5a91905157386a1e4db4ed2e98
Khristopher Samora <kj_samora@Roux-3.local>
2017-09-14 16:29:26 -0700

    adding files to be written

---
+++ js/game.js
@@ -0,0 +1,13 @@
+function continueStory() {
+  document.getElementById('title').innerHTML = "Hello World!";
+  document.getElementById('title').style.fontSize = "39px";
+}
+function exit() {
+  var answer = confirm("Are you sure you want to exit?")
+  if (Yes) {
+    //link
+  }
+  else {
+    //close dialog box
+  }
+}
